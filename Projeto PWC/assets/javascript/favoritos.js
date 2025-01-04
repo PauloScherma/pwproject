e14b7c786d55234d75f3fc822629a49e3fa1166f
Paulo Scherma <pauloantonioschermapinto@gmail.com>
2025-01-04 15:27:00 +0000

    manter os card sempre com a mesma height

--- Projeto PWC/assets/javascript/favoritos.js
+++ Projeto PWC/assets/javascript/favoritos.js
@@ -16,7 +16,7 @@ favorites.forEach(countryName => {
         card.innerHTML =
             `<div class="card h-100 rounded-0 px-4 pt-4 card-shadow rounded-3">
       <a href="detalhes.html?country=${country.name.common}">
-        <img src="${country.flags.png}" class="card-img-top" alt="Bandeira de ${country.name.common}">
+        <img src="${country.flags.png}" class="card-img-top card-height" alt="Bandeira de ${country.name.common}">
       </a>
       <div class="card-body d-flex justify-content-center align-items-center">
         <h2 class="card-title text-center d-inline">${country.name.common}</h2>
